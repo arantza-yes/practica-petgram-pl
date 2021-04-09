3c859e8192cf87d62cf569eaee00cd4c0dd52953
arantza-yes <yeseniaarantza@gmail.com>
2021-04-09 15:44:15 -0500

    deploy with vercel

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 ReactDom.render(
-  <h1>seguimos con react avanzado</h1>,
+  <h1>seguimos con react avanzado!!!!</h1>,
   document.getElementById('app')
 )
